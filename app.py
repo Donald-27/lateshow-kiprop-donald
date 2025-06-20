918c02afa54e385a5b7018daaa8eef663f87981f
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 23:15:06 +0300

    Set up SQLAlchemy for database handling

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Create the Flask application
 app = Flask(__name__)
-
 app.config.from_object('config.Config')
 
 db = SQLAlchemy(app)
