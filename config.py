d05fc41cb33b4379dfae78f69411e1c129c344cb
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 23:37:57 +0300

    Added relationship from episode to appearance with cascade delete

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-git add config.py
-git commit -m "Create config.py file with header comment"
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
--- models.py
+++ models.py
@@ -1,7 +1,61 @@
+# models.py - Contains database models for Episode, Guest, and Appearance
+
 from app import db
+
 class Episode(db.Model):
+    __tablename__ = 'episodes'
+
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship('Appearance', backref='episode', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "number": self.number,
+            "appearances": [appearance.to_dict() for appearance in self.appearances]
+        }
+
+
 class Guest(db.Model):
+    __tablename__ = 'guests'
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "occupation": self.occupation
+        }
+
+
 class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
     id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "guest_id": self.guest_id,
+            "episode_id": self.episode_id,
+            "guest": self.guest.to_dict(),
+            "episode": {
+                "id": self.episode.id,
+                "date": self.episode.date,
+                "number": self.episode.number
+            }
+        }
