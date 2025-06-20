df4e7053433387b7271219d9d54868af1c90c597
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 23:46:32 +0300

    Added separate method to_dict_with_appearances to episode for full detail route

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from app import db
 
 class Episode(db.Model):
     __tablename__ = 'episodes'
-
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String, nullable=False)
     number = db.Column(db.Integer, nullable=False)
@@ -69,7 +68,7 @@ class Appearance(db.Model):
             }
         }
 
-    # Used in nested appearance lists (e.g. inside episode)
+    # Nested appearance
     def to_dict_basic(self):
         return {
             "id": self.id,
