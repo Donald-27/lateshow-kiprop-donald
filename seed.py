53c437228c0cff78064dc13c9eaa26106fb27338
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 23:50:12 +0300

    Add all API routes in seed.py

--- seed.py
+++ seed.py
@@ -1,7 +1,32 @@
-from app import app, db
-from models import Episode, Guest, Appearance
+from flask import request, jsonify
 
-with app.app_context():
-    pass
-    db.drop_all()
-    db.create_all()
+@app.route('/episodes', methods=['GET'])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([e.to_dict() for e in episodes]), 200
+
+@app.route('/episodes/<int:id>', methods=['GET'])
+def get_episode_by_id(id):
+    episode = Episode.query.get(id)
+    if episode:
+        return jsonify(episode.to_dict_with_appearances()), 200
+    return jsonify({"error": "Episode not found"}), 404
+
+@app.route('/guests', methods=['GET'])
+def get_guests():
+    guests = Guest.query.all()
+    return jsonify([g.to_dict() for g in guests]), 200
+@app.route('/appearances', methods=['POST'])
+def create_appearance():
+    data = request.get_json()
+    try:
+        appearance = Appearance(
+            rating=data['rating'],
+            guest_id=data['guest_id'],
+            episode_id=data['episode_id']
+        )
+        db.session.add(appearance)
+        db.session.commit()
+        return jsonify(appearance.to_dict()), 201
+    except Exception as e:
+        return jsonify({"errors": [str(e)]}), 400
